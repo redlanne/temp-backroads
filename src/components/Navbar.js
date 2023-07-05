f69541e68011e5abaebe85366b489ec60b3c457c
Anne Redwood <anne.redwood@uk.ibm.com>
2023-07-05 08:49:56 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import logo from '../images/logo.svg';
+import { pageLinks, socialLinks } from '../data';
+import PageLink from './PageLink';
+import SocialLink from './SocialLink';
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <img src={logo} className="nav-logo" alt="backroads" />
+            <button type="button" className="nav-toggle" id="nav-toggle">
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+
+          <ul className="nav-links" id="nav-links">
+            {pageLinks.map((link) => {
+              return <PageLink key={link.id} {...link} itemClass="nav-link" />;
+            })}
+          </ul>
+
+          <ul className="nav-icons">
+            {socialLinks.map((link) => {
+              return (
+                <SocialLink key={link.id} {...link} itemClass="nav-icon" />
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+export default Navbar;
